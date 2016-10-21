6b51773e39816627bf6c5e38d503b5cd6b1c3f86
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2016-10-21 16:26:57 -0700

    tone() works much better

--- solenoid-firmware/solenoid-firmware.ino
+++ solenoid-firmware/solenoid-firmware.ino
@@ -3,26 +3,24 @@ byte stepPin = 11;
 byte resetPin = A1;
 
 unsigned long waitTime = 10;
+int currentNote = 440;
 
 void setup() {
   Serial.begin(115200);
-  Serial.setTimeout(2);
+  Serial.setTimeout(20);
   pinMode(directionPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
   pinMode(resetPin, OUTPUT);
   digitalWrite(directionPin, HIGH);
+  digitalWrite(resetPin, HIGH);
 }
 
 void loop() {
   int t = Serial.parseInt();
-  if (t > 0) {
-    waitTime = 1000/t;
+  if (currentNote != t && t > 50) {
+    tone(stepPin, t);
+    currentNote = t;
   }
-  digitalWrite(resetPin, HIGH);
-  digitalWrite(stepPin, HIGH);
-  delay(waitTime);
-  digitalWrite(stepPin, LOW);
-  digitalWrite(resetPin, LOW);
 }
 
 
